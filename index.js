519b8843a165778633e7a99620dc6d5832d832e7
Amprayil Joel Oommen <843463@students.wits.ac.za>
2018-04-25 21:20:55 +0200

    Updated Code that Actually Works

--- index.js
+++ index.js
@@ -1,28 +1,11 @@
-//let path = require ('path');
-//let express = require ('express');
-//let mainRouter = express.Router ();
-
-//mainRouter.get('/', function (req, res){
-//  res.send ('Hello World');
-//});
-
-//mainRouter.get('/about', function (req, res){
-//  res.sendFile(path.join(__dirname, 'views', 'about.html'));
-//});
-
-//app.listen(3000);
-//console.log("Express server running on port 3000");
 let path = require('path');
 let express = require('express');
+let dirpath = require('path')
 let app = express();
+let mainRouter= require('./mainRoutes.js')
 
-app.get('/', function (req, res) {
-res.send('Hello World');
-});
 
-app.get('/about', function (req, res){
-  res.sendFile(path.join(__dirname, 'views', 'about.html'));
-});
+app.use('/', mainRouter);
 
 app.listen(3000);
 console.log("Express server running on port 3000");
